797497fc17ae996116b48c18e8708222dc607772
cheungmine <350137278@qq.com>
2020-02-27 20:08:05 +0800

    vscode build for win32 static lib ok

---
+++ vscode-msvc-x64.bat
@@ -0,0 +1,2 @@
+@echo setup msvc build env for x64 (Win32 64-bits) module.
+cmd /K "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
---
+++ vscode-msvc-x86.bat
@@ -0,0 +1,2 @@
+@echo setup msvc build env for x86 (Win32 32-bits) module.
+cmd /K "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
