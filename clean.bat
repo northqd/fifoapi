25a9b0fc039916d12669a254a99240094769142c
cheungmine <350137278@qq.com>
2020-02-28 12:26:04 +0800

    msvc build app and lib success

---
+++ clean.bat
@@ -0,0 +1,8 @@
+cd /d %~dp0
+del fifoserver.obj
+del fifoclient.obj
+del vc140.pdb
+del build\fifoserver.exe
+del build\fifoclient.exe
+del build\*.obj
+del build\*.lib
---
+++ examples/fifoserver.c
@@ -0,0 +1,48 @@
+/**
+ * @filename   fifoserver.c
+ *   Sample server shows how to use fifo api.
+ *
+ * @author     Liang Zhang <350137278@qq.com>
+ * @version    0.0.1
+ * @create     2020-01-21 21:09:05
+ * @update     2020-01-22 15:09:05
+ */
+#include "../src/cstrbuf.h"
+#include "../src/fifo.h"
+
+#ifdef __WINDOWS__
+    # include <Windows.h>
+
+    # include "win32/getoptw.h"
+#else
+    // Linux: see Makefile
+    # include <getopt.h>
+#endif
+
+
+#define APPNAME        "logserver"
+#define APPVER         "0.0.1"
+
+fifo_server server = 0;
+
+static void appexit_cleanup(void)
+{
+    if (server) {
+        fifo_server_free(server);
+    }
+}
+
+
+int main (int argc, char *argv[])
+{
+    WINDOWS_CRTDBG_ON
+
+    /* register on exit cleanup */
+    atexit(appexit_cleanup);
+
+    if (fifo_server_new(0, FIFO_TIMEOUT, FIFO_CONNECT_TIMEOUT, &server) == 0) {
+        fifo_server_runforever(server, fifoReplyForRequest, 0);
+    }
+
+    return 0;
+}
