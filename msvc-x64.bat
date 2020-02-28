aa4a52d87d281a26f1e522b913886c3465066e69
pepstack <350137278@qq.com>
2020-02-28 23:52:53 +0800

    rename msvc bat

--- msvc-x64.bat
+++ msvc-x64.bat
@@ -1,2 +1,3 @@
 @echo setup msvc build env for x64 (Win32 64-bits) module.
+cd /d %~dp0 && mkdir .\build
 cmd /K "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
--- msvc-x86.bat
+++ msvc-x86.bat
@@ -1,2 +1,3 @@
 @echo setup msvc build env for x86 (Win32 32-bits) module.
+cd /d %~dp0 && mkdir .\build
 cmd /K "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
